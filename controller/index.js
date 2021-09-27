a38704bca6d4610b387b7f67aa7088785d76b3f2
Cayman Gill <cayman.g@hotmail.com>
2021-09-26 20:55:26 -0400

    added routes and templates for edit and dashboard

--- controller/index.js
+++ controller/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homepageRoutes = require('./hompageRoutes')
+const homepageRoutes = require('./hompageRoutes');
+const dashboardRoutes = require('./dashboard');
 
 router.use('/api', apiRoutes);
-router.use('/', homepageRoutes)
+router.use('/dashboard', dashboardRoutes);
+router.use('/', homepageRoutes);
 
 module.exports = router;
